3abb493092f9c1333f377069406515e30407c6c0
saaiid <ibrosakid@gmail.com>
2023-03-08 19:06:34 -0500

    add tables using Group By and Having

--- 2_queries_joins/total-time.sql
+++ 2_queries_joins/total-time.sql
@@ -21,3 +21,49 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+
+-- If we want to calculate the total number of assignment_submissions for all students, 
+-- the count aggregate function is perfect.
+
+SELECT count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions;
+
+/* But what if we wanted to calculate the total number of assignment_submissions for each student 
+individually, and output the totals next to the student's name?
+We want to apply the count() function to groups of data, rather than the entire query. The groups 
+in this case are the names of students. We want to apply the count() function for each students.name. */
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id;
+
+-- Now we just need to tell SQL that we want count(assignment_submissions.*) to be run for each 
+-- students.name, instead of the entire query. To do this, we use the GROUP BY clause.
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+GROUP BY students.name;
+
+-- Alter the query slightly to only return currently enrolled students.
+-- A currently enrolled student has a null end_date.
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
+
+-- HAVING
+-- Now that we know how to get the total submissions for currently enrolled students, 
+-- let's refine the query a bit more. Let's only return currently enrolled students 
+-- who's total submissions  are less than 100;
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
+
